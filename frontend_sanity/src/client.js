60f791e6d5b5940e3105dd7d82458ec5ecb43e38
Mandeep Singhmar <msingh8328@gmail.com>
2021-12-08 13:36:38 +0530

    added env for projectId

--- frontend_sanity/src/client.js
+++ frontend_sanity/src/client.js
@@ -2,12 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: '60slbd68',
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
-  apiVersion: '2021-11-16', // use current UTC date - see "specifying API version"!
-  useCdn: true, // `false` if you want to ensure fresh data
-  token:
-  process.env.REACT_APP_SANITY_TOKEN,
+  apiVersion: '2021-11-16',
+  useCdn: true,
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
